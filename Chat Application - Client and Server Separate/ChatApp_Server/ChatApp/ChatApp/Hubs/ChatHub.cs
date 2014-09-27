fb6e3fd29ce3cd280746075c9b8bc16b96880986
yogeshl <yogeshlcse@hotmail.com>
2014-09-27 21:23:55 +0530

    Chat Application
    Client and Server applications are separated.
    Both client and server should be in same domain.

---
+++ Chat Application - Client and Server Separate/ChatApp_Server/ChatApp/ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.NewMessageCallback(name, message);
+        }
+    }
+}
